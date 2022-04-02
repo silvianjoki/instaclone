27ae6332f704caf11e3934dc8affe837f8595633
silvianjoki <10silvianjoki@gmail.com>
2022-04-02 13:42:26 +0300

    add bootstrap

--- clone/views.py
+++ clone/views.py
@@ -7,5 +7,5 @@ from django.http  import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Welcome to the homepage')
+    return render(request, 'index.html')
 
