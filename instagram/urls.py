102956a89a2b5972ee58ccbcc503c4ff2001d24c
silvianjoki <10silvianjoki@gmail.com>
2022-04-02 16:26:28 +0300

    add login

--- instagram/urls.py
+++ instagram/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(r'', include('clone.urls'))
+    path('', include('clone.urls')),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+
 ]
