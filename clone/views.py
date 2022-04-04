15e9f4c175e5e25a4ae14dfc113655d79e153627
silvianjoki <10silvianjoki@gmail.com>
2022-04-04 15:26:19 +0300

    add templates

--- clone/views.py
+++ clone/views.py
@@ -1,11 +1,14 @@
 
+from distutils.command.upload import upload
+from django.contrib.auth.models import User
 from turtle import title
 from django.shortcuts import redirect, render
 from django.http  import HttpResponse, HttpResponseRedirect, Http404
 from django.contrib.auth.decorators import login_required
 from .forms import ProfileForm,UploadImageForm, CommentForm
 from .models import Image, Profile, Comments
-# from django.contrib.auth.mixins import LoginRequiredMixin
+
+
 
 
 
@@ -23,21 +26,44 @@ def index(request):
 
 @login_required(login_url='/accounts/login/')
 def upload_image(request):
-    title = "Instagram | Upload image"
     current_user = request.user
-    try:
-        profile = Profile.objects.get(user = current_user)
-    except Profile.DoesNotExist:
-        raise Http404()
     if request.method == "POST":
         form = UploadImageForm(request.POST, request.FILES)
         if form.is_valid():
             image = form.save(commit=False)
-            image.profile = profile
+            image.profile = current_user
             image.save()
         return redirect('index')
     else:
         form = UploadImageForm()
-    return render(request, 'upload_image.html', {'form': form, 'title': title})
+    return render(request, 'upload_image.html', {'form': form, })
     
+    
+@login_required(login_url='/accounts/login/')
+def profile(request):
+    current_user = request.user
+    if request.method == ' POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.name = current_user
+            profile.save()
+        else:
+            form = ProfileForm()
+            
+        return render (request, 'profile/new_profile.html')
+    
+
+@login_required(login_url='/accounts/login/')
+def search(request):
+    profile_list = User.objects.all()
+
+    if 'name' in request.GET and request.GET['name']:
+        searched_name = request.GET.get('name')
+        results = User.objects.filter(username__icontains=searched_name)
+        print(results)
+
+        return render(request,'results.html',locals())
+
+
     
